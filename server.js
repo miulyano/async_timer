d76cb3d9623b41c3e64670fc7317d75b8aa5c860
Maxim <ulyanov2022@gmail.com>
2018-08-20 09:36:39 +0300

    fix server

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const http = require('http');
 const interval = process.env.INTERVAL || 1000;
-const stop = process.env.STOP || 1000;
+const stop = process.env.STOP || 3000;
 const serverPort = process.env.PORT || 3000;
-let timer;
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    timer = setInterval(() => {
-      console.log(new Date().toUTCString());
-    }, interval);
-    setTimeout(() => {
-      clearInterval(timer);
-      res.end(`<h1>server stop <h2>(${new Date().toUTCString()})</h2></h1>`);
-    }, stop);
+  if (req.url === '/favicon.ico') {
+    return;
   }
+  let timer = setInterval(() => {
+    console.log(new Date().toUTCString());
+  }, interval);
+  setTimeout(() => {
+    clearInterval(timer);
+    timer = null;
+    res.end(`<h1>server stop <h2>(${new Date().toUTCString()})</h2></h1>`);
+  }, stop);
 });
 
 server.listen(serverPort, (err) => {
